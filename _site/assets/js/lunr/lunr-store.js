e69d0213955f33d0d29ecceacf37ca5298bb4b13
binh nguyen quang <binh.nq@teko.vn>
2021-07-24 02:12:54 +0700

    First

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "About Me",
+        "excerpt":"When I started to create this blog at 2021-07-24, I was: a 28th-year-old dad with a newborn baby, I love her and her mom so much. a Data Scientist at Teko(VNPAY, DigiLife whatever…) for more than 1 year. in COVID pandemic’s 2nd year and thought about human’s future. Any one...","categories": [],
+        "tags": [],
+        "url": "/2021/07/24/about-me.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2021/07/24/welcome-to-jekyll.html",
+        "teaser": null
+      }]
